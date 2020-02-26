33bbcff53c326fbe63f0527b86dcc345a558f925
Aleksey <hoopengo@gmail.com>
2020-02-26 19:27:01 +0300

    Create Parser.py

---
+++ Parser.py
@@ -0,0 +1,18 @@
+import random
+import requests
+import os
+
+i = 0
+
+def parse(i):
+	while True:
+		url = "http://static.donationalerts.ru/audiodonations/"
+		a = random.randint(11111, 99999)
+		b = random.randint(111,999)
+		url = url + str(a) + '/'
+		url = url + str(a) + str(b) + ".wav"
+		session = requests.Session()
+		request = session.get(url)
+		if request.status_code != 404:
+			print(url)
+parse(i)
