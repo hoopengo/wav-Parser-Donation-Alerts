adebef9a9a12f41777e46db531a69c09e911b24d
Aleksey <hoopengo@gmail.com>
2020-02-29 23:06:20 +0300

    Update project.py

--- project.py
+++ project.py
@@ -1,5 +1,12 @@
 import random
 import requests
+import os
+print('Введите название папки со сливами:')
+path = input()
+try:
+	os.mkdir(path)
+except OSError:
+	pass
 def parse():
 	while True:
 		url = "http://static.donationalerts.ru/audiodonations/"
@@ -10,7 +17,8 @@ def parse():
 		s = requests.Session()
 		r = s.get(url, stream=True)
 		if r.status_code == 200:
-			with open(str(random.randint(1,1000000)) + '.wav', 'wb') as f:
+			with open(str(path + '/' + random.randint(1,1000000)) + '.wav', 'wb') as f:
 				f.write(r.content)
 			print(url)
 parse()
+print('Подождите... Это займёт около минуты!')
