f810c92ae5d78b35b09e13379989b90a2483d9d7
Aleksey <hoopengo@gmail.com>
2022-01-22 23:55:10 +0300

    Update project.py
    мне лень писать нормально, но и прошлое я оставить не мог

--- project.py
+++ project.py
@@ -1,23 +1,22 @@
-import random
-import requests
-import os
-print('Выберите в какую папку будут сохранятся .wav фалы (Авто-Создание):')
-path = input()
+import os, random, requests
+
+
+pth = input("Введите папку: ")
 try:
-	os.mkdir(path)
+	os.mkdir(pth)
+	print("*Папка выбрана*")
 except OSError:
-	pass
+	print("*Папка выбрана*")
+
 def parse():
 	while True:
-		url = "http://static.donationalerts.ru/audiodonations/"
-		a = random.randint(11111, 99999)
-		b = random.randint(111,999)
-		url = url + str(a) + '/'
-		url = url + str(a) + str(b) + ".wav"
-		r = requests.get(url, stream=True)
+		url = f"http://static.donationalerts.ru/audiodonations/{random.randint(11111, 99999)}/{random.randint(111,999)}.wav"
+		r = requests.get(url)
 		if r.status_code == 200:
-			with open(path + '/' + str(random.randint(1,1000000)) + '.wav', 'wb') as f:
+			with open(pth + '/' + str(random.randint(1,1000000)) + '.wav', 'wb') as f:
 				f.write(r.content)
-			print(url)
+			print(url + " YES")
+		else:
+			print(url + " NOT")
 print('Подождите... Это займёт около минуты!')
 parse()
